3b810f20f1483cbf89afd293f9dc802bd18183a5
Andre Alves <andrealvesdeoliveira@outlook.com>
2020-07-24 18:36:19 -0300

    Finalizado os exercícios de Loops em JavaScript

--- loops.ex04.js
+++ loops.ex04.js
@@ -1,7 +1,7 @@
 /*
 Exercício: Loops com Array - Lucros
 
-UUma empresa separou em uma lista, os valores dos lucros mensais. 
+Uma empresa separou em uma lista, os valores dos lucros mensais. 
 Com isso você terá no código um array com o seguinte nome: listaDeLucro contendo em cada posição o valor de recebido de cada mês!
 
 var listaDeLucro = [100, 30, 300, -10, 600, 10]
