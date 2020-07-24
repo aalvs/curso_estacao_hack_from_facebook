adf50a5c806f42bdac2f0565208e171d6d92e951
Andre Alves <andrealvesdeoliveira@outlook.com>
2020-07-24 14:06:38 -0300

    Finalizado exercícios de variáveis

---
+++ variaveis_ex03.js
@@ -0,0 +1,13 @@
+/* 
+Exercício: Tipos de dados
+
+Escreva no script as variáveis expostas abaixo substituindo os tipos de dados por valores, conforme vimos anteriormente. 
+
+var idade = Number
+var sobrenome = String
+var salarioMinimo = Float 
+*/
+
+var idade = 25
+var sobrenome = 'Alves'
+var salarioMinimo = 1.045
