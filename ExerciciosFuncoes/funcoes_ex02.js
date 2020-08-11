9b646e4e0a874b239227d2c83861dd1b5fee2d0e
Andre Alves <andrealvesdeoliveira@outlook.com>
2020-08-11 17:23:50 -0300

    Finalizado as atividades de Funções

---
+++ ExerciciosFuncoes/funcoes_ex02.js
@@ -0,0 +1,14 @@
+/*
+Exercício: Declarando funções - Cartão de visita
+
+Crie uma função chamada cartaoDeVisitas, ela deverá imprimir o seu nome em conjunto com sobrenome! 
+*/
+
+function cartaoDeVisitas( ) {
+
+  var nome  = 'André'
+  var sobrenome = 'Alves'
+
+  console.log(nome + sobrenome)
+  
+}
